ab03a50f4d86c98fcab49b1d729f3932b61ccb50
boo <studiiesboo@gmail.com>
2026-02-12 16:58:27 -0300

    commit

--- section2/aula12 variaveis com let/index2.js
+++ section2/aula12 variaveis com let/index2.js
@@ -3,3 +3,7 @@ nome = 'João'; // inicializando a variavel
 console.log(nome);
 nome = 'Otavio';
 console.log(nome);
+
+// variaveis são case sensitive. e utilizam camelCase
+// não podemos redeclarar variaveis com o let.
+// NÃO usar VAR, sempre usar LET (para criar variaveis que se alteram)
---
+++ section2/aula14 constantes const/index.js
@@ -0,0 +1,6 @@
+// não podemos modificar o valor de constantes, elas precisam ser criadas e ja atribuidas o valor na criação.
+// não usar FOR, UTILIZE CONST!!
+
+const nome = 'João'; //igual a variavel com LET
+console.log(nome);
+
